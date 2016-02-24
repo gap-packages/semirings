636ac6b6d4f7cf4549ea8f8c7145eb2dd3f838a3
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2016-02-24 16:11:02 +0000

    Define some semirings

--- gap/semiring.gd
+++ gap/semiring.gd
@@ -4,6 +4,36 @@
 # Declarations
 #
 
+#T Note that at the moment GAP's semirings are not associative,
+#T and hence we're fobbing it a bit here
+
 #! @Description
-#!   Insert documentation for you function here
-DeclareGlobalFunction( "semirings_Example" );
+#!
+DeclareCategory( "IsIdempotentSemiring", IsSemiring );
+
+#! @Description
+#!
+DeclareCategory( "IsOrderedSemiring", IsSemiring );
+
+#! @Description
+#!
+DeclareCategory( "IsAntiinvolutiveSemiring",
+                 IsIdempotentSemiring );
+
+DeclareCategory( "IsSemiringElement",
+                 IsAdditiveElement and IsMultiplicativeElement );
+
+DeclareCategory( "IsAntiinvolutiveSemiringElement",
+                 IsSemiringElement  and IsAntiinvolutiveObject );
+
+# A semiring conjugate on an ordered semiring is an operation such that
+# * for all a SemiringConjugate(SemiringConjugate(a)) = a
+# * ca < b implies SemiringConjugate(b)c < SemiringConjugate(a)
+# (David Wilding's axioms)
+DeclareOperation( "SemiringConjugate"
+                , [ IsAntiinvolutiveSemiringElement ] );
+
+# This will usually be defined to be SemiringConjuate(a)*a
+DeclareOperation( "Residuate"
+                , [ IsAntiinvolutiveSemiringElement, IsSemiringElement ] );
+
